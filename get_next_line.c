3f18c6c9a4da7e7f45a22112e41e2aed90abaa0c
Tabasco39 <ranaivoantsaa@gmail.com>
2024-03-25 04:48:56 +0300

    rectif for get next line function

--- get_next_line.c
+++ get_next_line.c
@@ -18,22 +18,20 @@
 #define BUFFER_SIZE 100
 #endif
 
-char *manage_stocked(char **data_stocked)
+char *manage_stocked(char **data_stocked, int *read_value)
 {
 	char *result;
 	char *temp_content;
 
 	temp_content = NULL;
 	result = ft_strdup("");
-
-	if (*data_stocked && check_nl(*data_stocked) != -1)
+	if (*data_stocked )
 	{
-		result = ft_substr(*data_stocked, 0, check_nl(*data_stocked) + 1);
-		*data_stocked = ft_substr(*data_stocked, check_nl(*data_stocked) + 1, ft_strlen(*data_stocked) - check_nl(*data_stocked));
-	}
-	else if (*data_stocked)
-	{
-		result = ft_join_until_nl(result, *data_stocked);
+		if (check_nl(*data_stocked) != -1)
+		{
+			result = ft_substr(*data_stocked, 0, check_nl(*data_stocked) + 1);
+			*data_stocked = ft_substr(*data_stocked, check_nl(*data_stocked) + 1, ft_strlen(*data_stocked) - check_nl(*data_stocked));
+		}
 	}
 	return (result);
 }
@@ -71,9 +69,11 @@ char *get_next_line(int fd)
 	if (!(result))
 		read_value = 1;
 	result = ft_strdup("");
-	result = manage_stocked(&data_stocked);
-	if (data_stocked && check_nl(data_stocked) != -1)
-		return (result);
+	if (data_stocked && (check_nl(data_stocked) != -1))
+	 	return (manage_stocked(&data_stocked, &read_value));
+	if (data_stocked)
+		result = ft_join_until_nl(result, data_stocked);
+
 	if (read_value == 0 || fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	while (read_value != 0)
@@ -83,7 +83,8 @@ char *get_next_line(int fd)
 			return (NULL);
 		read_value = read(fd, temp_content, BUFFER_SIZE);
 		result = get_only_read_char(temp_content, &result, read_value);
-		return (free(temp_content), get_only_line(&data_stocked, &result, read_value));
+		if (check_nl(result) != -1)
+			return (free(temp_content), get_only_line(&data_stocked, &result, read_value));
 	}
 	return (result);
 }
@@ -100,8 +101,11 @@ int main(void)
 		{
 			// free(res);
 			res = get_next_line(fd);
-			printf("==> %s", res);
+			printf("%s", res);
 		}
+		// get_next_line(fd);
+		// get_next_line(fd);
+		// get_next_line(fd);
 	}
 	free(res);
 }
